f2b45fc548fac50f7f98d6ba352956940ab985f9
EricBM19 <ericbm19@gmail.com>
2025-09-22 12:42:52 +0200

    Doc updated and Client Class updated.

--- docs/rentingCarv1.0.md
+++ docs/rentingCarv1.0.md
@@ -23,3 +23,46 @@ public class Car
     //Constructor, getters & setters, methods and toString
 }
 ```
+
+### Class Client
+
+```java
+public class Client
+{
+    private String id;
+    private String name;
+    private String lastName;
+    private String address;
+    private String email;
+    private boolean premium;
+
+    //Constructor, getters & setters and toString
+}
+```
+
+### Class Booking
+
+```java
+public class Booking
+{
+    private String id;
+    private Car car;
+    private int days;
+    private double price;
+    private boolean isActive;
+
+    //Constructor, getters & setters and toString
+}
+```
+
+---
+
+## Tech Stack
+
+---
+
+- IDE: IntelliJ IDEA 2025.2.1 (Community Edition)
+
+- Java 21
+
+- Maven Project: maven-archetype-quickstart
--- rentingCarTest/src/main/java/org/example/Client.java
+++ rentingCarTest/src/main/java/org/example/Client.java
@@ -4,10 +4,90 @@ public class Client
 {
     private String id;
     private String name;
+    private String lastName;
+    private String address;
+    private String email;
+    private boolean premium;
 
-    public Client(String id, String name)
+    public Client(String id, String name, String lastName, String address, String email, boolean premium)
     {
         this.id = id;
         this.name = name;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.premium = premium;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public boolean isPremium()
+    {
+        return premium;
+    }
+
+    public void setPremium(boolean premium)
+    {
+        this.premium = premium;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", premium=" + premium +
+                '}';
     }
 }
