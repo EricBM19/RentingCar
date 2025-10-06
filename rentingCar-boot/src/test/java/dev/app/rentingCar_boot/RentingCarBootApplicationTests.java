68f7cba4733ebbf504926f93d0faa22063ac25ee
EricBM19 <ericbm19@gmail.com>
2025-10-06 09:55:01 +0200

    Code: Spring boot project creation

---
+++ rentingCar-boot/src/test/java/dev/app/rentingCar_boot/RentingCarBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.app.rentingCar_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentingCarBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
