573bd32e3e8628681ad698961945e93b32fd373c
EricBM19 <ericbm19@gmail.com>
2025-10-21 14:05:34 +0200

    Code: GenerateUnvailableDateHashMap value changed.

--- rentingCar-boot/src/main/java/dev/app/rentingCar_boot/utils/GenerateUnavailableDateHashMap.java
+++ rentingCar-boot/src/main/java/dev/app/rentingCar_boot/utils/GenerateUnavailableDateHashMap.java
@@ -9,7 +9,7 @@ public class GenerateUnavailableDateHashMap {
 
         Map<Integer, Boolean> oneYearDatesHashMap = new HashMap<>();
 
-        for (int i = 1767225600; i <= 1798675200; i = i+86400) {
+        for (int i = 1767268800; i <= 1798718400; i = i+86400) {
 
             oneYearDatesHashMap.put(i,true);
         }
